564ec618b9dd3dca86fe4ffec3258a643b0c038a
Jhoni Giacomolli <edicao@microsite.net.br>
2023-05-28 02:37:26 -0300

    chore: update jest config
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- jest.config.ts
+++ jest.config.ts
@@ -2,19 +2,18 @@ export default {
   roots: ['<rootDir>/src'],
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/src/domain'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/src/domain',
+    '<rootDir>/src/protocols',
+    '<rootDir>/src/configurations'
+  ],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
