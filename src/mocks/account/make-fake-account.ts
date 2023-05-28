48679de01047241f7335a75c931ec7d2a7773e07
Jhoni Giacomolli <edicao@microsite.net.br>
2023-05-28 02:36:42 -0300

    feat: create mocks folder
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

---
+++ src/mocks/account/make-fake-account.ts
@@ -0,0 +1,18 @@
+import { AccountModel } from "@/protocols/models/account";
+
+export const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password',
+  address: 'valid_address',
+  image: 'valid_image',
+  teamId: 'valid_team_id',
+  phone: 'valid_phone',
+  boards: [],
+  desktops: [],
+  permissions: [],
+  responsability: [],
+  status: [],
+  tasks: [],
+})
