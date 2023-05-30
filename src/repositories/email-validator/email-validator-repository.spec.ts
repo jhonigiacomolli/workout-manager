70176e04ebb3e96c990ca66fbed6784623199c2b
Jhoni Giacomolli <edicao@microsite.net.br>
2023-05-30 01:17:32 -0300

    test: email balidator should return true if valid email is provided
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/email-validator/email-validator-repository.spec.ts
+++ src/repositories/email-validator/email-validator-repository.spec.ts
@@ -1,4 +1,5 @@
 import { EmailValidatorRepository } from "./email-validator-repository"
+import validator from 'email-validator'
 
 const makeSut = () => {
   const sut = new EmailValidatorRepository()
@@ -10,7 +11,14 @@ const makeSut = () => {
 describe('Email Validator Repository', () => {
   test('Should return false if invalid email is provided', () => {
     const { sut } = makeSut()
+    jest.spyOn(validator, 'validate').mockReturnValueOnce(false)
     const isValid = sut.validate('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
+  test('Should return true if valid email is provided', () => {
+    const { sut } = makeSut()
+    const isValid = sut.validate('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
 })
+
