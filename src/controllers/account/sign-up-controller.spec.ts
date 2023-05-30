7344e1fabcc1bb87ef363be126b686f059701b36
Jhoni Giacomolli <edicao@microsite.net.br>
2023-05-29 22:46:18 -0300

    test: controller return 400 when invalid email is provided
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/account/sign-up-controller.spec.ts
+++ src/controllers/account/sign-up-controller.spec.ts
@@ -1,52 +1,72 @@
-import { httpError, httpRequest } from "@/helpers/http"
-import { makeFakeAccount } from "@/mocks/account/make-fake-account"
 import { SignUpController } from "./sign-up-controller"
+import { makeFakeAccount } from "@/mocks/account/make-fake-account"
+import { httpError, httpReponse, httpRequest } from "@/helpers/http"
+import { EmailValidator } from "@/protocols/models/validator/email-validator"
 
 const makeSut = () => {
-  const fakeAccount = httpRequest({
+  const fakeRequest = httpRequest({
     ...makeFakeAccount(),
     passwordConfirmation: makeFakeAccount().password,
   })
-  const sut = new SignUpController()
+
+  class EmailValidatorStub implements EmailValidator {
+    validate(email: string) {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+
+  const sut = new SignUpController({
+    emailValidator: emailValidatorStub,
+  })
+
   return {
     sut,
-    fakeAccount,
+    emailValidatorStub,
+    fakeRequest,
   }
 }
 
 describe('Account Controller', () => {
-  test('Should controller called with correct values', async () => {
-    const { sut, fakeAccount } = makeSut()
-    const controller = await sut.handle(fakeAccount)
+  test('Should controller called with correct values', () => {
+    const { sut, fakeRequest } = makeSut()
+    const controller = sut.handle(fakeRequest)
 
-    expect(controller).toEqual(fakeAccount)
+    expect(controller).toEqual(httpReponse(200, fakeRequest))
   })
   test('Should controller return 400 with e-mail not provided', async () => {
-    const { sut, fakeAccount } = makeSut()
-    fakeAccount.body.email = ''
-    const controller = sut.handle(fakeAccount)
+    const { sut, fakeRequest } = makeSut()
+    fakeRequest.body.email = ''
+    const controller = sut.handle(fakeRequest)
 
-    expect(controller).resolves.toEqual(httpError(400, `Invalid param: email`))
+    expect(controller).toEqual(httpError(400, `Invalid param: email`))
   })
   test('Should controller return 400 with password not provided', async () => {
-    const { sut, fakeAccount } = makeSut()
-    fakeAccount.body.password = ''
-    const controller = sut.handle(fakeAccount)
+    const { sut, fakeRequest } = makeSut()
+    fakeRequest.body.password = ''
+    const controller = sut.handle(fakeRequest)
 
-    expect(controller).resolves.toEqual(httpError(400, `Invalid param: password`))
+    expect(controller).toEqual(httpError(400, `Invalid param: password`))
   })
   test('Should controller return 400 with password confimation not provided', async () => {
-    const { sut, fakeAccount } = makeSut()
-    fakeAccount.body.passwordConfirmation = ''
-    const controller = sut.handle(fakeAccount)
+    const { sut, fakeRequest } = makeSut()
+    fakeRequest.body.passwordConfirmation = ''
+    const controller = sut.handle(fakeRequest)
 
-    expect(controller).resolves.toEqual(httpError(400, `Invalid param: passwordConfirmation`))
+    expect(controller).toEqual(httpError(400, `Invalid param: passwordConfirmation`))
   })
   test('Should controller return 400 when password not equal to password confirmation', async () => {
-    const { sut, fakeAccount } = makeSut()
-    fakeAccount.body.passwordConfirmation = 'other_password'
-    const controller = sut.handle(fakeAccount)
+    const { sut, fakeRequest } = makeSut()
+    fakeRequest.body.passwordConfirmation = 'other_password'
+    const controller = sut.handle(fakeRequest)
 
-    expect(controller).resolves.toEqual(httpError(400, `Invalid param: password`))
+    expect(controller).toEqual(httpError(400, `Invalid param: password`))
+  })
+  test('Should controller return 400 when email is invalid', async () => {
+    const { sut, emailValidatorStub, fakeRequest } = makeSut()
+    jest.spyOn(emailValidatorStub, 'validate').mockReturnValueOnce(false)
+    const controller = sut.handle(fakeRequest)
+    expect(controller).toEqual(httpError(400, `Invalid param: email`))
   })
 })
