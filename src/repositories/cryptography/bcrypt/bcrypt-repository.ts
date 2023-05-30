9fb52030222bd6dee6e0303797f53a17cdbd3557
Jhoni Giacomolli <edicao@microsite.net.br>
2023-05-30 01:43:28 -0300

    feat: ensure bcrypt repository is called and resturn correct values
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

---
+++ src/repositories/cryptography/bcrypt/bcrypt-repository.ts
@@ -0,0 +1,14 @@
+import { Cryptography } from "@/protocols/use-cases/cryptography";
+import { hash } from "bcrypt";
+
+export class BcryptRepository {
+  constructor(private readonly salt: number) { }
+  // encrypt(email: string, passoword: string): Promise<string> {
+
+  //   return Promise.resolve('')
+  // }
+  async hash(password: string): Promise<string> {
+    const hashResult = await hash(password, this.salt)
+    return hashResult
+  }
+}
