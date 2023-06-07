f44c291d98a47b36ebc6eabf9be3efc98308428f
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-06 22:32:15 -0300

    feat: setup sign up route on express
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 import 'dotenv/config'
 import './configurations/module-alias'
 
-const main = () => console.log('Main')
-main()
+import express, { json } from 'express'
+import { SignUpController } from './controllers/account/sign-up-controller'
+import { PgAccountRepository } from './repositories/acccount/postgres-account-repository'
+import { EmailValidatorRepository } from './repositories/email-validator/email-validator-repository'
+import { BcryptRepository } from './repositories/cryptography/hasher/bcrypt/bcrypt-hasher-repository'
+
+const app = express()
+
+app.use(json())
+
+app.post('/sign-up', async (req, res) => {
+  const params = req.body
+  const account = new PgAccountRepository()
+  const emailValidator = new EmailValidatorRepository()
+  const hasher = new BcryptRepository(12)
+  console.log('Params: ',  params)
+  const controller = new SignUpController({ account, emailValidator, hasher })
+  const httpResponse = await controller.handle(req || {})
+
+  res.status(httpResponse.statusCode).json(httpResponse.body)
+})
+
+app.listen(3008, () => console.log('Server is running on https://localhost:3008'))
