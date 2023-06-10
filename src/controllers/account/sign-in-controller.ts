eb24bef748616653e8bd6c5990258818eca54d35
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-10 09:10:45 -0300

    feat: check if username is a valid account
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/account/sign-in-controller.ts
+++ src/controllers/account/sign-in-controller.ts
@@ -1,8 +1,15 @@
 import { httpResponse } from "@/helpers/http";
 import { Controller } from "@/protocols/models/controller"
 import { HTTPRequest, HTTPResponse } from "@/protocols/models/http";
+import { Account } from "@/protocols/use-cases/account";
+
+type ConstructorProps = {
+  account: Account
+}
 
 export class SignInController implements Controller {
+  constructor (private readonly dependencies: ConstructorProps) {}
+
   async handle(request: HTTPRequest): Promise<HTTPResponse> {
     const requiredParams = ['username', 'password']
 
@@ -12,6 +19,12 @@ export class SignInController implements Controller {
         if (!request.body[param]) {
           return httpResponse(400, `Empty param: ${param} is required`)
         }
+      }
+
+      const isValidUser = await this.dependencies.account.checkEmailInUse(username)
+
+      if(!isValidUser) {
+        return httpResponse(404, 'user not found')
       }
 
     return Promise.resolve(httpResponse(200, 'success'))
