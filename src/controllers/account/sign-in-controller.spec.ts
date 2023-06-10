d4be017dfcc585b6a38a92f20173a5939a5bf689
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-10 09:10:54 -0300

    test: check if username is a valid account
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/account/sign-in-controller.spec.ts
+++ src/controllers/account/sign-in-controller.spec.ts
@@ -1,9 +1,23 @@
 import { httpRequest, httpResponse } from "@/helpers/http";
 import { SignInController } from "./sign-in-controller";
 import { response } from "express";
+import { Account } from "@/protocols/use-cases/account";
 
 const makeSut = () => {
-  const sut = new SignInController()
+  class AccountStub implements Account {
+    async create(): Promise<boolean> {
+      return Promise.resolve(true)
+    }
+
+    async checkEmailInUse(): Promise<boolean> {
+      return Promise.resolve(false)
+    }
+  }
+
+  const accountStub = new AccountStub()
+  const sut = new SignInController({
+    account: accountStub
+  })
   const fakeRequest = httpRequest({
     username: 'any_username',
     password: 'any_password'
@@ -11,6 +25,7 @@ const makeSut = () => {
   return {
     sut,
     fakeRequest,
+    accountStub,
   }
 }
 
@@ -33,8 +48,14 @@ describe('Sign in', () => {
        body: {
          ...fakeRequest.body,
          password: '',
-     }
-   })
+      }
+    })
     expect(response).toEqual(httpResponse(400, 'Empty param: password is required'))
+  })
+  test('Should return 404 if username is not registed', async () => {
+    const { sut, fakeRequest, accountStub } = makeSut()
+    const response = await sut.handle(fakeRequest)
+    jest.spyOn(accountStub, 'checkEmailInUse').mockImplementationOnce(() => { return Promise.resolve(false) })
+    expect(response).toEqual(httpResponse(404, 'user not found'))
   });
 });
