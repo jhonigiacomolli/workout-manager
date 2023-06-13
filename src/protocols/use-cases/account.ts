1331bd44133d25746da3f4022e44431d5d5f1fc7
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-12 23:23:04 -0300

    feat: change return possible undefined on account getUserByEmail method
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/protocols/use-cases/account.ts
+++ src/protocols/use-cases/account.ts
@@ -5,5 +5,5 @@ export type CreateAccountParams = Omit<AccountModel, 'id'>
 export interface Account {
   create: (account: CreateAccountParams) => Promise<boolean>
   checkEmailInUse: (email: string) => Promise<boolean>
-  getUserByEmail: (email: string) => Promise<AccountModel>
+  getUserByEmail: (email: string) => Promise<AccountModel | undefined>
 }
