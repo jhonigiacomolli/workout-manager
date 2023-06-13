baffb28ccca3b9aed3ead4639418611c042e439b
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-12 23:03:02 -0300

    feat: create make sign in controller factory
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

---
+++ src/helpers/factories/controllers/make-sign-in-controller.ts
@@ -0,0 +1,12 @@
+import { SignInController } from '@/controllers/account/sign-in-controller'
+import { PgAccountRepository } from '@/repositories/acccount/postgres-account-repository'
+import { BcryptRepository } from '@/repositories/cryptography/hasher/bcrypt/bcrypt-hasher-repository'
+import { JsonwebtokenRepository } from '@/repositories/cryptography/encrypter/jsonwebtoken/jsonwebtoken-repository'
+
+export const makeSignInController = () => {
+  return new SignInController({
+    account: new PgAccountRepository(),
+    hasher: new BcryptRepository(12),
+    encrypter: new JsonwebtokenRepository(),
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import './configurations/module-alias'
 import express, { json } from 'express'
 import { useRouteController } from './helpers/http'
 import { makeSignUpController } from './helpers/factories/controllers'
+import { makeSignInController } from './helpers/factories/controllers/make-sign-in-controller'
 
 const app = express()
 
 app.use(json())
 
 app.post('/sign-up', useRouteController(makeSignUpController()))
+app.post('/sign-in', useRouteController(makeSignInController()))
 
 app.listen(3008, () => { console.log('Server is running on https://localhost:3008') })
