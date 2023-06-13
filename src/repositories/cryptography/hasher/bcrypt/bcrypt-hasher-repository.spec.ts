6ab9628f761e8d191a4c5b1cc7ca3fa56a2cc12c
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-12 22:47:31 -0300

    test: implements bcrypt compare method on bcrypt hasher repository
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/cryptography/hasher/bcrypt/bcrypt-hasher-repository.spec.ts
+++ src/repositories/cryptography/hasher/bcrypt/bcrypt-hasher-repository.spec.ts
@@ -7,7 +7,7 @@ const makeSut = () => ({
 })
 
 describe('Bcrypt Repository', () => {
-  describe('Hash', () => {
+  describe('generate()', () => {
     test('Should Bcrypt Repository calls generate medthod with correct password', async () => {
       const { sut, salt } = makeSut()
       const hashSpy = jest.spyOn(bcrypt, 'hash')
@@ -24,6 +24,32 @@ describe('Bcrypt Repository', () => {
       const { sut } = makeSut()
       jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => { await Promise.reject(new Error()) })
       const hashResult = sut.generate('any_password')
+      return expect(hashResult).rejects.toThrow()
+    })
+  })
+  describe('compare()', () => {
+    test('Should Bcrypt Repository calls compare medthod with correct password', async () => {
+      const { sut } = makeSut()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.compare('any_password', 'hashed_password')
+      expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
+    })
+    test('Should Bcrypt Repository return true if password and hash to be equal', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => await Promise.resolve(true))
+      const hashResult = await sut.compare('any_password', 'hashed_password')
+      expect(hashResult).toBeTruthy()
+    })
+    test('Should Bcrypt Repository return false if password and hash not to be equal', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => await Promise.resolve(false))
+      const hashResult = await sut.compare('any_password', 'hashed_password')
+      expect(hashResult).toBeFalsy()
+    })
+    test('Should Bcrypt Repository throw if compare method throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => { await Promise.reject(new Error()) })
+      const hashResult = sut.compare('any_password', 'hashed_password')
       return expect(hashResult).rejects.toThrow()
     })
   })
