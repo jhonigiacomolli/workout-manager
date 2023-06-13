db8d07ca1466202bf5851913a4e4c573202e4b90
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-12 22:47:20 -0300

    feat: implements bcrypt compare method on bcrypt hasher repository
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/cryptography/hasher/bcrypt/bcrypt-hasher-repository.ts
+++ src/repositories/cryptography/hasher/bcrypt/bcrypt-hasher-repository.ts
@@ -1,11 +1,16 @@
-import { hash } from 'bcrypt'
+import { compare, hash } from 'bcrypt'
 import { type Hasher } from '@/protocols/use-cases/cryptography/hashser'
 
 export class BcryptRepository implements Hasher {
-  constructor (private readonly salt: number) { }
+  constructor(private readonly salt: number) { }
 
-  async generate (password: string): Promise<string> {
+  async generate(password: string): Promise<string> {
     const hashResult = await hash(password, this.salt)
     return hashResult
+  }
+
+  async compare(password: string, hash: string): Promise<boolean> {
+    const compareResult = await compare(password, hash)
+    return compareResult
   }
 }
