d4cbd3e27925703e22b7b45be89dda0aaca0b704
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-19 00:44:11 -0300

    test: implement decrypt method on jswnwebtoken repository
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/cryptography/encrypter/jsonwebtoken/jsonwebtoken-repository.spec.ts
+++ src/repositories/cryptography/encrypter/jsonwebtoken/jsonwebtoken-repository.spec.ts
@@ -1,23 +1,52 @@
 import jwt from 'jsonwebtoken'
 import { JsonwebtokenRepository } from './jsonwebtoken-repository'
 
+jest.mock('jsonwebtoken', () => ({
+  verify: jest.fn(() => {
+    return { id: 'decrypted_id' }
+  }),
+  sign: jest.fn(() => {
+    return 'encrypted_hash'
+  }),
+}))
+
 const makeSut = () => new JsonwebtokenRepository()
 
 process.env.JWT_SECURE_KEY = 'secret-key'
 
 describe('Json WebToken Repository', () => {
-  test('Should Repository is called with correct value', async () => {
-    const sut = makeSut()
-    const jsonSpy = jest.spyOn(jwt, 'sign')
-    const token = await sut.encrypt('any_id')
-    expect(jsonSpy).toHaveBeenCalledWith('any_id', 'secret-key')
-    expect(typeof token).toBe('string')
-    expect(token).not.toBe('')
-  })
+  describe('encrypt()', () => {
+    test('Should Repository is called with correct value', async () => {
+      const sut = makeSut()
+      const jsonSpy = jest.spyOn(jwt, 'sign')
+      const token = await sut.encrypt('any_id')
+      expect(jsonSpy).toHaveBeenCalledWith('any_id', 'secret-key')
+      expect(typeof token).toBe('string')
+      expect(token).not.toBe('')
+    })
 
-  test('Should Repository return 500 with secret is not set', async () => {
-    const sut = makeSut()
-    delete process.env.JWT_SECURE_KEY
-    await expect(sut.encrypt('any_id')).rejects.toThrow('')
+    test('Should Repository return 500 with secret is not set', async () => {
+      const sut = makeSut()
+      delete process.env.JWT_SECURE_KEY
+      await expect(sut.encrypt('any_id')).rejects.toThrow('')
+    })
+  })
+  describe('decrypt', () => {
+    test('Should Repository is called with correct value', async () => {
+      process.env.JWT_SECURE_KEY = 'secret-key'
+
+      const sut = makeSut()
+      const jsonSpy = jest.spyOn(jwt, 'verify')
+      const token = await sut.decrypt('hashed_token')
+
+      expect(jsonSpy).toHaveBeenCalledWith('hashed_token', 'secret-key')
+      expect(token).not.toBe('')
+    })
+
+    test('Should Repository throws id secret is not set', async () => {
+      const sut = makeSut()
+      delete process.env.JWT_SECURE_KEY
+      await expect(sut.decrypt('hashed_token')).rejects.toThrow('')
+    })
   })
 })
