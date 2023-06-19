0d7d2cf5e2ef3268998e1a0e16834686f80cbed4
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-19 00:43:45 -0300

    feat: implement decrypt method on jswnwebtoken repository
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/cryptography/encrypter/jsonwebtoken/jsonwebtoken-repository.ts
+++ src/repositories/cryptography/encrypter/jsonwebtoken/jsonwebtoken-repository.ts
@@ -1,9 +1,9 @@
 import { env } from 'process'
-import { sign } from 'jsonwebtoken'
+import { sign, verify } from 'jsonwebtoken'
 import { type Encrypter } from '@/protocols/use-cases/cryptography/encrypter'
 
 export class JsonwebtokenRepository implements Encrypter {
-  async encrypt (id: string): Promise<string> {
+  async encrypt(id: string): Promise<string> {
     const secret = env.JWT_SECURE_KEY
 
     if (!secret) throw new Error('')
@@ -11,5 +11,15 @@ export class JsonwebtokenRepository implements Encrypter {
     const encrypted = sign(id, secret)
 
     return await Promise.resolve(encrypted)
+  }
+
+  async decrypt(hash: string): Promise<{ id: string }> {
+    const secret = env.JWT_SECURE_KEY
+
+    if (!secret) throw new Error('')
+
+    const decrypted = verify(hash, secret) as string
+
+    return await Promise.resolve({ id: decrypted })
   }
 }
