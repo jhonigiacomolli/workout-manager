c1bab2f23c02be008816ac6a158a2898cd4f0518
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-22 11:53:08 -0300

    feat: encrypt id using options sended by controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/cryptography/encrypter/jsonwebtoken/jsonwebtoken-repository.ts
+++ src/repositories/cryptography/encrypter/jsonwebtoken/jsonwebtoken-repository.ts
@@ -1,25 +1,39 @@
 import { env } from 'process'
-import { sign, verify } from 'jsonwebtoken'
-import { type Encrypter } from '@/protocols/use-cases/cryptography/encrypter'
+import { JwtPayload, sign, verify } from 'jsonwebtoken'
+import { EncryptOptions, type Encrypter } from '@/protocols/use-cases/cryptography/encrypter'
 
+type JWTEncryptOptions = {
+  expiresIn: number
+  issuer?: string
+}
 export class JsonwebtokenRepository implements Encrypter {
-  async encrypt(id: string): Promise<string> {
+  async encrypt(id: string, options?: EncryptOptions): Promise<string> {
     const secret = env.JWT_SECURE_KEY
+    const encriptOtions: JWTEncryptOptions = {
+      expiresIn: 3600,
+    }
+
+    if (options?.expire) {
+      encriptOtions.expiresIn = options.expire
+    }
+    if (options?.issuer) {
+      encriptOtions.issuer = options.issuer
+    }
 
     if (!secret) throw new Error('')
 
-    const encrypted = sign(id, secret)
+    const encrypted = sign({ data: { id } }, secret, encriptOtions)
 
     return await Promise.resolve(encrypted)
   }
 
-  async decrypt(hash: string): Promise<{ id: string }> {
+  async decrypt(hash: string): Promise<any> {
     const secret = env.JWT_SECURE_KEY
 
     if (!secret) throw new Error('')
 
-    const decrypted = verify(hash, secret) as string
+    const decrypted = verify(hash, secret) as JwtPayload
 
-    return await Promise.resolve({ id: decrypted })
+    return await Promise.resolve(decrypted.data)
   }
 }
