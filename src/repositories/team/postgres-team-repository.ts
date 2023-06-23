7997341114083d18f3869f86b558257de5bdad98
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-23 17:11:18 -0300

    refactor: handle erro on ps team repository
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/team/postgres-team-repository.ts
+++ src/repositories/team/postgres-team-repository.ts
@@ -4,8 +4,12 @@ import { Team } from '@/protocols/use-cases/team'
 
 export class PgTeamRepository implements Team {
   async getTeamByID(id: string): Promise<TeamModel | undefined> {
-    const { rows } = await client.query('SELECT * FROM teams WHERE id=$1', [id])
+    try {
+      const { rows } = await client.query('SELECT * FROM teams WHERE id=$1', [id])
 
-    return rows[0]
+      return rows[0]
+    } catch {
+      return undefined
+    }
   }
 }
