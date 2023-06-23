3b06a1d69ce5d4e2edbc3a96815a0e0a4328014e
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-23 14:16:31 -0300

    test: remove unused dependencies on account update controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/account/account-update/account-update-controller.spec.ts
+++ src/controllers/account/account-update/account-update-controller.spec.ts
@@ -1,9 +1,7 @@
 import { httpRequest, httpResponse } from '@/helpers/http'
 import { AccountStub } from '@/mocks/account/account-stub'
-import { HasherStub } from '@/mocks/cryptography/hasher-stub'
 import { makeFakeAccount } from '@/mocks/account/make-fake-account'
 import { AccountUdateController } from './account-update-controller'
-import { EncrypterStub } from '@/mocks/cryptography/encrypter-stub'
 
 const makeSut = () => {
   const body = {
@@ -16,59 +14,19 @@ const makeSut = () => {
   const fakeRequest = httpRequest(body, headers)
 
   const accountStub = new AccountStub()
-  const hasherStub = new HasherStub()
-  const encrypterStub = new EncrypterStub()
 
   const sut = new AccountUdateController({
     account: accountStub,
-    hasher: hasherStub,
-    encrypter: encrypterStub,
   })
 
   return {
     sut,
     accountStub,
-    hasherStub,
-    encrypterStub,
     fakeRequest,
   }
 }
 
 describe('Account Update Controller', () => {
-  test('Should return 401 if accessToken is not provided on header', async () => {
-    const { sut, fakeRequest } = makeSut()
-    const result = await sut.handle({
-      ...fakeRequest,
-      headers: {
-        authorization: '',
-      },
-    })
-    expect(result).toEqual(httpResponse(401, 'Unauthorized'))
-  })
-  test('Should return 401 if accessToken is not a valid token', async () => {
-    const { sut, encrypterStub, fakeRequest } = makeSut()
-
-    jest.spyOn(encrypterStub, 'decrypt').mockReturnValueOnce(Promise.resolve({ id: '' }))
-
-    const result = await sut.handle({
-      ...fakeRequest,
-      headers: {
-        authorization: 'invalid_token',
-      },
-    })
-    expect(result).toEqual(httpResponse(401, 'Unauthorized'))
-  })
-  test('Should return 500 if decrypt method throws', async () => {
-    const { sut, encrypterStub, fakeRequest } = makeSut()
-
-    jest.spyOn(encrypterStub, 'decrypt').mockImplementationOnce(() => {
-      throw new Error()
-    })
-
-    const result = await sut.handle(fakeRequest)
-
-    expect(result).toEqual(httpResponse(500, 'Internal Server Error'))
-  })
   test('Should return 400 if setUserById method return false', async () => {
     const { sut, accountStub, fakeRequest } = makeSut()
 
