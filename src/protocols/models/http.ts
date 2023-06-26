54ef167e43be748275ad197cf8407213b03b3cb8
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-26 10:27:10 -0300

    feat: create http request params model
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/protocols/models/http.ts
+++ src/protocols/models/http.ts
@@ -1,11 +1,20 @@
-export interface HTTPRequest {
+export type HTTPRequestParams = {
+  [key: string]: string | undefined
+  host?: string
+  page?: string
+  limit?: string
+  sort?: string
+  order?: string
+}
+
+export type HTTPRequest = {
   body?: any
-  params?: any
+  params?: HTTPRequestParams
   headers?: any
   header?: any
 }
 
-export interface HTTPResponse {
+export type HTTPResponse = {
   statusCode: number
   body: any
 }
