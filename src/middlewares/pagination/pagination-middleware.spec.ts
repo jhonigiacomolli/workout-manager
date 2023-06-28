5ccc4241c7f2c0369b2575edb3e8503ce1f9dc43
Jhoni Giacomolli <edicao@microsite.net.br>
2023-06-28 18:30:48 -0300

    test: create pagination middleware
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

---
+++ src/middlewares/pagination/pagination-middleware.spec.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from 'express'
+import { pagination } from './pagination-middleware'
+
+const fakeRequest: Partial<Request> = {
+  query: {},
+}
+
+const fakeResponse: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+}
+
+const fakeNext = jest.fn()
+
+describe('PaginationMiddleWare', () => {
+  test('Should inject default pagination params if no query param is provided', async () => {
+    await pagination(fakeRequest as Request, fakeResponse as Response, fakeNext)
+    expect(fakeRequest).toEqual({
+      ...fakeRequest,
+      query: {
+        pagination: {
+          limit: '10',
+          page: '1',
+          order: 'desc',
+          sort: 'id',
+        },
+      },
+    })
+    expect(fakeNext).toHaveBeenCalled()
+  })
+  test('Should inject correct pagination params if limit query param is provided', async () => {
+    fakeRequest.query = {
+      limit: '20',
+      page: '2',
+      order: 'asc',
+      sort: 'name',
+    }
+
+    await pagination(fakeRequest as Request, fakeResponse as Response, fakeNext)
+
+    expect(fakeRequest).toEqual({
+      ...fakeRequest,
+      query: {
+        ...fakeRequest.query,
+        pagination: {
+          limit: '20',
+          page: '2',
+          order: 'asc',
+          sort: 'name',
+        },
+      },
+    })
+  })
+  test('Should inject correct pagination params if limit query param id -1', async () => {
+    fakeRequest.query = {
+      limit: '-1',
+    }
+
+    await pagination(fakeRequest as Request, fakeResponse as Response, fakeNext)
+
+    expect(fakeRequest).toEqual({
+      ...fakeRequest,
+      query: {
+        ...fakeRequest.query,
+        pagination: {
+          limit: undefined,
+          page: '1',
+          order: 'desc',
+          sort: 'id',
+        },
+      },
+    })
+  })
+})
