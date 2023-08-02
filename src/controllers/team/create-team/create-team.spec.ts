40c481ee6982b62bda3a42f25a528d513874d9ba
Jhoni Giacomolli <edicao@microsite.net.br>
2023-08-01 22:28:31 -0300

    team: create create team controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

---
+++ src/controllers/team/create-team/create-team.spec.ts
@@ -0,0 +1,49 @@
+import { httpRequest, httpResponse } from '@/helpers/http'
+import { CreateTeamController } from './create-team'
+import { TeamStub } from '@/mocks/teams/team-stub'
+import { makeFakeTeam } from '@/mocks/teams/make-fake-team'
+
+const fakeRequest = httpRequest({
+  name: 'valid_team_title',
+})
+const fakeEmptyRequest = httpRequest({})
+
+const makeSut = () => {
+  const teamStub = new TeamStub()
+  const sut = new CreateTeamController({
+    team: teamStub,
+  })
+
+  return {
+    teamStub,
+    sut,
+  }
+}
+
+describe('Create Team Controller', () => {
+  test('Should return an http response', async () => {
+    const { sut } = makeSut()
+    const result = await sut.handle(fakeRequest)
+    expect(result).toHaveProperty('statusCode')
+    expect(result).toHaveProperty('body')
+  })
+  test('Should return 200 if team created succesfully', async () => {
+    const { sut } = makeSut()
+    const result = await sut.handle(fakeRequest)
+    expect(result).toEqual(httpResponse(200, {
+      message: 'Successfully registered team',
+      data: makeFakeTeam(),
+    }))
+  })
+  test('Should return 400 if title param is not provided', async () => {
+    const { sut } = makeSut()
+    const result = await sut.handle(fakeEmptyRequest)
+    expect(result).toEqual(httpResponse(400, 'Empty param: name is required'))
+  })
+  test('Should return 500 if method throws when create new team', async () => {
+    const { sut, teamStub } = makeSut()
+    jest.spyOn(teamStub, 'create').mockImplementationOnce(() => { throw new Error() })
+    const result = await sut.handle(fakeRequest)
+    expect(result).toEqual(httpResponse(500, 'Internal Server Error'))
+  })
+})
