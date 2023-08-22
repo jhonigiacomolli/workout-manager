360ccbc54655b219a2e2d68989ddb0bcd3608a23
Jhoni Giacomolli <edicao@microsite.net.br>
2023-08-22 11:16:51 -0300

    refactor: change from return httpResponse to throw an custom error on sign in controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/account/sing-in/sign-in-controller.ts
+++ src/controllers/account/sing-in/sign-in-controller.ts
@@ -5,6 +5,7 @@ import { type HTTPRequest, type HTTPResponse } from '@/protocols/models/http'
 import { type Encrypter } from '@/protocols/use-cases/cryptography/encrypter'
 
 import { httpResponse } from '@/helpers/http'
+import { BadRequestError, NotFoundError } from '@/helpers/errors'
 
 interface ConstructorProps {
   account: Account
@@ -16,44 +17,40 @@ export class SignInController implements Controller {
   constructor(private readonly dependencies: ConstructorProps) { }
 
   async handle(request: HTTPRequest): Promise<HTTPResponse> {
-    try {
-      const requiredParams = ['email', 'password']
+    const requiredParams = ['email', 'password']
 
-      const { email, password } = request.body
+    const { email, password } = request.body
 
-      for (const param of requiredParams) {
-        if (!request.body[param]) {
-          return httpResponse(400, `Empty param: ${param} is required`)
-        }
+    for (const param of requiredParams) {
+      if (!request.body[param]) {
+        throw new BadRequestError(`Empty param: ${param} is required`)
       }
-
-      const isValidUser = await this.dependencies.account.checkEmailInUse(email)
-
-      if (!isValidUser) {
-        return httpResponse(404, 'user not found')
-      }
-
-      const user = await this.dependencies.account.getUserByEmail(email)
-
-      if (!user) {
-        return httpResponse(404, 'user not found')
-      }
-
-      const isCorrectPassword = await this.dependencies.hasher.compare(password, user.password)
-
-      if (!isCorrectPassword) {
-        return httpResponse(403, 'wrong password')
-      }
-
-      const accessToken = await this.dependencies.encrypter.encrypt({ id: user.id }, { expire: 3600, issuer: request.headers.host })
-      const refreshToken = await this.dependencies.encrypter.encrypt({ id: user.id }, { expire: 86400, issuer: request.headers.host })
-
-      return httpResponse(200, {
-        accessToken,
-        refreshToken,
-      })
-    } catch (err) {
-      return httpResponse(500, 'Internal Server Error')
     }
+
+    const isValidUser = await this.dependencies.account.checkEmailInUse(email)
+
+    if (!isValidUser) {
+      throw new NotFoundError('user not found')
+    }
+
+    const user = await this.dependencies.account.getUserByEmail(email)
+
+    if (!user) {
+      throw new NotFoundError('user not found')
+    }
+
+    const isCorrectPassword = await this.dependencies.hasher.compare(password, user.password)
+
+    if (!isCorrectPassword) {
+      throw new BadRequestError('wrong password')
+    }
+
+    const accessToken = await this.dependencies.encrypter.encrypt({ id: user.id }, { expire: 3600, issuer: request.headers.host })
+    const refreshToken = await this.dependencies.encrypter.encrypt({ id: user.id }, { expire: 86400, issuer: request.headers.host })
+
+    return httpResponse(200, {
+      accessToken,
+      refreshToken,
+    })
   }
 }
