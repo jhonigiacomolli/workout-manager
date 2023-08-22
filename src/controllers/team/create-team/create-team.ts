a372744401dcac1c9e5d15c62b203f094bb5705f
Jhoni Giacomolli <edicao@microsite.net.br>
2023-08-22 11:47:14 -0300

    refactor: change from return httpResponse to throw an custom error on create team controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/team/create-team/create-team.ts
+++ src/controllers/team/create-team/create-team.ts
@@ -1,7 +1,8 @@
 import { httpResponse } from '@/helpers/http'
+import { Team } from '@/protocols/use-cases/team'
+import { BadRequestError } from '@/helpers/errors'
 import { Controller } from '@/protocols/models/controller'
 import { HTTPRequest, HTTPResponse } from '@/protocols/models/http'
-import { Team } from '@/protocols/use-cases/team'
 
 type ConstructorProps = {
   team: Team
@@ -9,25 +10,21 @@ type ConstructorProps = {
 export class CreateTeamController implements Controller {
   constructor(private readonly dependencies: ConstructorProps) { }
   async handle(request: HTTPRequest): Promise<HTTPResponse> {
-    try {
-      const { name = '', members = [] } = request.body
+    const { name = '', members = [] } = request.body
 
-      if (!name) {
-        return httpResponse(400, 'Empty param: name is required')
-      }
-
-      const data = await this.dependencies.team.create({
-        name,
-        members,
-        createdAt: new Date().toISOString(),
-      })
-
-      return httpResponse(200, {
-        message: 'Successfully registered team',
-        data,
-      })
-    } catch {
-      return httpResponse(500, 'Internal Server Error')
+    if (!name) {
+      throw new BadRequestError('Empty param: name is required')
     }
+
+    const data = await this.dependencies.team.create({
+      name,
+      members,
+      createdAt: new Date().toISOString(),
+    })
+
+    return httpResponse(200, {
+      message: 'Successfully registered team',
+      data,
+    })
   }
 }
