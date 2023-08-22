65219a509fccf6a4def30542e336e12a46aca160
Jhoni Giacomolli <edicao@microsite.net.br>
2023-08-22 12:00:54 -0300

    test: change from return httpResponse to throw an custom error on load team controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/team/load-team/load-team.spec.ts
+++ src/controllers/team/load-team/load-team.spec.ts
@@ -1,6 +1,7 @@
 import { httpRequest, httpResponse } from '@/helpers/http'
 import { LoadTeamController } from './load-team'
 import { TeamStub } from '@/mocks/teams/team-stub'
+import { NotFoundError } from '@/helpers/errors'
 
 const fakeValidRequest = httpRequest({}, {}, {
   id: 'valid_team_id',
@@ -22,31 +23,28 @@ const makeSut = () => {
 describe('LoadTeamController', () => {
   test('Should return 400 if teamId is not provided', async () => {
     const { sut } = makeSut()
-    const result = await sut.handle(fakeInvalidRequest)
-    expect(result).toEqual(httpResponse(400, 'Required param teamId is not provided'))
 
-    const newResult = await sut.handle(fakeInvalidRequest)
-    expect(newResult).toEqual(httpResponse(400, 'Required param teamId is not provided'))
+    const result = sut.handle(fakeInvalidRequest)
+
+    await expect(result).rejects.toThrow(new NotFoundError('Required param teamId is not provided'))
   })
   test('Should return 404 if teamId is a invalid teamId', async () => {
     const { sut, teamStub } = makeSut()
 
     jest.spyOn(teamStub, 'getTeamByID').mockReturnValueOnce(Promise.resolve(undefined))
 
-    const result = await sut.handle(fakeValidRequest)
-    expect(result).toEqual(httpResponse(404, 'Team not found'))
+    const result = sut.handle(fakeValidRequest)
+    await expect(result).rejects.toThrow(new NotFoundError('Team not found'))
   })
   test('Should return 200 with team data if succeeds', async () => {
     const { sut } = makeSut()
     const result = await sut.handle(fakeValidRequest)
 
     expect(result).toEqual(httpResponse(200, {
-      data: {
-        id: 'valid_team_id',
-        name: 'any_name',
-        createdAt: '2023-06-30T03:00:00.000Z',
-        members: [],
-      },
+      id: 'valid_team_id',
+      name: 'any_name',
+      createdAt: '2023-06-30T03:00:00.000Z',
+      members: [],
     }))
   })
 })
