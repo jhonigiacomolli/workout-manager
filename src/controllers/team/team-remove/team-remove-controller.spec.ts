1ac656983d60d387e41f524e7a7308640b179fb2
Jhoni Giacomolli <edicao@microsite.net.br>
2023-08-24 08:50:07 -0300

    test: create team remove controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

---
+++ src/controllers/team/team-remove/team-remove-controller.spec.ts
@@ -0,0 +1,63 @@
+import { TeamStub } from '@/mocks/teams/team-stub'
+import { BadRequestError } from '@/helpers/errors'
+import { httpRequest, httpResponse } from '@/helpers/http'
+import { TeamRemoveController } from './team-remove-controller'
+
+const makeSut = () => {
+  const requestBody = {}
+  const requestHeaders = {
+    authorization: 'valid_access_token',
+  }
+  const requestParams = {
+    id: 'any_account_id',
+  }
+  const fakeRequest = httpRequest(requestBody, requestHeaders, requestParams)
+  const fakeRequestWithoutId = httpRequest(requestBody, requestHeaders, {})
+  const teamStub = new TeamStub()
+  const sut = new TeamRemoveController({
+    team: teamStub,
+  })
+
+  return {
+    sut,
+    fakeRequest,
+    fakeRequestWithoutId,
+    teamStub,
+  }
+}
+
+describe('TeamRemoveController', () => {
+  test('Should return 400 if teamId is not provided', async () => {
+    const { sut, fakeRequestWithoutId } = makeSut()
+
+    const output = sut.handle(fakeRequestWithoutId)
+
+    await expect(output).rejects.toThrow(new BadRequestError('Empty param: id is required'))
+  })
+  test('Should calls delete mothod with teamId', async () => {
+    const { sut, fakeRequest, teamStub } = makeSut()
+
+    const teamSpy = jest.spyOn(teamStub, 'delete')
+
+    await sut.handle(fakeRequest)
+
+    expect(teamSpy).toHaveBeenCalledWith(fakeRequest.params.id)
+    expect(teamSpy).not.toHaveBeenCalledWith('other_value')
+  })
+  test('Should return 400 if delete method return false', async () => {
+    const { sut, fakeRequest, teamStub } = makeSut()
+
+    jest.spyOn(teamStub, 'delete').mockReturnValueOnce(Promise.resolve(false))
+
+    const result = sut.handle(fakeRequest)
+
+    await expect(result).rejects.toThrow(new BadRequestError('Team removal failed!'))
+  })
+  test('Should return 204 if proccess succeeds', async () => {
+    const { sut, fakeRequest } = makeSut()
+
+    const result = await sut.handle(fakeRequest)
+
+    expect(result).toEqual(httpResponse(200, 'Team removed'))
+  })
+})
