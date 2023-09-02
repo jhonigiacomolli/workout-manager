c68a455d1689cc58f3761ab3442c98376c06224a
Jhoni Giacomolli <edicao@microsite.net.br>
2023-09-02 10:35:53 -0300

    test: create getAll method on postgres workspace repository
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/workspace/postgres-workspace-repository.spec.ts
+++ src/repositories/workspace/postgres-workspace-repository.spec.ts
@@ -14,6 +14,22 @@ jest.mock('pg', () => {
   return { Client: jest.fn(() => mClient) }
 })
 
+const fakeRequestParams = {
+  host: 'localhost',
+  page: '1',
+  limit: '10',
+  offset: '0',
+  order: 'DESC',
+  orderBy: 'name',
+}
+
+const querySql = `
+      SELECT * FROM workspaces
+      ORDER BY name DESC
+      LIMIT $1
+      OFFSET $2:: integer * $1:: integer;
+    `
+
 const makeSut = () => {
   const sut = new PgWorkspaceReposytory()
   const { id, ...params } = makeFakeWorkspace()
@@ -45,6 +61,47 @@ describe('PostgresWorkspaceReposytory', () => {
       const { sut, params } = makeSut()
       const result = await sut.create(params)
       expect(result).toEqual(makeFakeWorkspace())
+    })
+  })
+  describe('getAll()', () => {
+    test('Should return an account model list if succeeds', async () => {
+      const { sut } = makeSut()
+
+      jest.spyOn(client, 'query').mockImplementationOnce(() => ({ rows: [makeFakePostgressWorkspace()] }))
+
+      const newResult = await sut.getAll(fakeRequestParams)
+      expect(newResult).toEqual([makeFakePostgressWorkspace()])
+    })
+    test('Should query is called with correct values', async () => {
+      const { sut } = makeSut()
+
+      const querySpy = jest.spyOn(client, 'query').mockImplementationOnce(() => ({ rows: [makeFakePostgressWorkspace()] }))
+
+      await sut.getAll(fakeRequestParams)
+
+      expect(querySpy).toHaveBeenCalledWith(querySql, ['10', '0'])
+
+      fakeRequestParams.page = '2'
+      fakeRequestParams.offset = '10'
+
+      await sut.getAll(fakeRequestParams)
+
+      expect(querySpy).toHaveBeenCalledWith(querySql, ['10', '10'])
+
+      fakeRequestParams.page = '5'
+      fakeRequestParams.offset = '40'
+
+      await sut.getAll(fakeRequestParams)
+
+      expect(querySpy).toHaveBeenCalledWith(querySql, ['10', '40'])
+    })
+    test('Should thorws an erro when account query fails', async () => {
+      const { sut } = makeSut()
+
+      jest.spyOn(client, 'query').mockImplementationOnce(() => { throw new Error() })
+
+      const result = sut.getAll(fakeRequestParams)
+      await expect(result).rejects.toThrow()
     })
   })
 })
