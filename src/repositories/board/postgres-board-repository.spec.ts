f072d90820289af11b2ed192bb95c8d15c4294ab
Jhoni Giacomolli <edicao@microsite.net.br>
2023-09-09 09:09:43 -0300

    test: cretae delete method on postgres board repository
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/repositories/board/postgres-board-repository.spec.ts
+++ src/repositories/board/postgres-board-repository.spec.ts
@@ -1,5 +1,5 @@
-import { PgBoardReposytory } from './postgres-board-repository'
 import { client } from '@/database'
+import { PgBoardReposytory } from './postgres-board-repository'
 import { makeFakeBoard, makePostgresFakeBoard } from '@/mocks/board/board-fakes'
 
 jest.mock('pg', () => {
@@ -58,36 +58,40 @@ describe('PostgresBoardReposytory', () => {
       const result = await sut.create(params)
       return expect(result).toBeUndefined()
     })
+
     test('Should throws when board register throws', async () => {
       const { sut, params } = makeSut()
       jest.spyOn(client, 'query').mockImplementationOnce(() => { throw new Error() })
       const result = sut.create(params)
       return expect(result).rejects.toThrow()
     })
+
     test('Should return board model if register succeeds', async () => {
       const { sut, params } = makeSut()
       const result = await sut.create(params)
       expect(result).toEqual(makeFakeBoard())
     })
   })
-  // describe('delete()', () => {
-  //   test('Should return true when team remove succeeds', async () => {
-  //     const { sut, paramsWithId } = makeSut()
-  //     const result = await sut.delete(paramsWithId.id)
-  //     expect(result).toBeTruthy()
-  //   })
-  //   test('Should return false id id is not provided', async () => {
-  //     const { sut } = makeSut()
-  //     const result = await sut.delete('')
-  //     expect(result).toBeFalsy()
-  //   })
-  //   test('Should return 500 when team remove fails', async () => {
-  //     const { sut, paramsWithId } = makeSut()
-  //     jest.spyOn(client, 'query').mockImplementationOnce(() => { throw new Error() })
-  //     const result = sut.delete(paramsWithId.id)
-  //     return expect(result).rejects.toThrow()
-  //   })
-  // })
+  describe('delete()', () => {
+    test('Should return true when team remove succeeds', async () => {
+      const { sut, paramsWithId } = makeSut()
+      const result = await sut.delete(paramsWithId.id)
+      expect(result).toBeTruthy()
+    })
+
+    test('Should return false id id is not provided', async () => {
+      const { sut } = makeSut()
+      const result = await sut.delete('')
+      expect(result).toBeFalsy()
+    })
+
+    test('Should return 500 when team remove fails', async () => {
+      const { sut, paramsWithId } = makeSut()
+      jest.spyOn(client, 'query').mockImplementationOnce(() => { throw new Error() })
+      const result = sut.delete(paramsWithId.id)
+      return expect(result).rejects.toThrow()
+    })
+  })
 
   describe('getAll()', () => {
     test('Should return an account model list if succeeds', async () => {
