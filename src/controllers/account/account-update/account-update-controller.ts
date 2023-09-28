8355aa0def09f824dcac33d71fdacda0e348f0f6
Jhoni Giacomolli <edicao@microsite.net.br>
2023-09-28 07:15:35 -0300

    feat: implement file upload on account update controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/account/account-update/account-update-controller.ts
+++ src/controllers/account/account-update/account-update-controller.ts
@@ -1,14 +1,16 @@
-import { type Account } from '@/protocols/use-cases/account'
+import { CreateAccountParams, type Account } from '@/protocols/use-cases/account'
 import { type Controller } from '@/protocols/models/controller'
 import { type HTTPRequest, type HTTPResponse } from '@/protocols/models/http'
 
 import { httpResponse } from '@/helpers/http'
 import { Team } from '@/protocols/use-cases/team'
 import { BadRequestError, EmptyParamError, InvalidParamError } from '@/helpers/errors'
+import { FileUploader } from '@/protocols/use-cases/file'
 
 interface ConstructorProps {
   account: Account
   team: Team
+  fileUploader: FileUploader
 }
 
 export class AccountUdateController implements Controller {
@@ -21,9 +23,12 @@ export class AccountUdateController implements Controller {
     if (!id) throw new EmptyParamError('id')
 
     const requiredParams = ['name', 'email']
+    const updatedParams: Partial<CreateAccountParams> = {}
 
     for (const param of requiredParams) {
       if (!request.body[param]) throw new EmptyParamError(`${param}`)
+
+      updatedParams[param] = request.body[param]
     }
 
     if (request.body.teamId) {
@@ -34,13 +39,22 @@ export class AccountUdateController implements Controller {
       request.body.teamId = undefined
     }
 
+    if (request.files.image) {
+      request.body.image = await this.dependencies.fileUploader.uploadImage(request.files.image)
+    }
+
     const updatedAccount = await this.dependencies.account.setUserById(id, request.body)
 
     if (!updatedAccount) throw new BadRequestError('Account update fails')
 
+    const updatedAccountWithImageUrl = {
+      ...updatedAccount,
+      image: request.baseUrl + updatedAccount.image,
+    }
+
     return httpResponse(200, {
       message: 'User updated successffuly',
-      data: updatedAccount,
+      data: updatedAccountWithImageUrl,
     })
   }
 }
--- src/mocks/file-uploader/file-uploader-stub.ts
+++ src/mocks/file-uploader/file-uploader-stub.ts
@@ -2,6 +2,6 @@ import { FileUploader } from '@/protocols/use-cases/file'
 
 export class FileUploaderStub implements FileUploader {
   async uploadImage(): Promise<string | null> {
-    return 'http://localhost/uploads/any-file-uploaded.png'
+    return '/uploads/any-file-uploaded.png'
   }
 }
