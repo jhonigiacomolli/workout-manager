880bc27c6c26e539606f3564b2d4c5d303d78849
Jhoni Giacomolli <edicao@microsite.net.br>
2023-10-05 07:35:28 -0300

    feat: inject local file manager on workspace remove factory
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/factories/controllers/workspace/make-workspace-remove-controller.ts
+++ src/factories/controllers/workspace/make-workspace-remove-controller.ts
@@ -1,9 +1,11 @@
 import { Controller } from '@/protocols/models/controller'
+import { LocalFileManagerRepository } from '@/repositories/file-manager/local-file-manager'
 import { PgWorkspaceReposytory } from '@/repositories/workspace/postgres-workspace-repository'
 import { WorkspaceRemoveController } from '@/controllers/workspace/workspace-remove/workspace-remove-controller'
 
 export const makeWorkspaceRemoveController = (): Controller => {
   return new WorkspaceRemoveController({
     workspace: new PgWorkspaceReposytory(),
+    fileManager: new LocalFileManagerRepository(),
   })
 }
