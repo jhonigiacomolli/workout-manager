238b2d54d9bf074e0e0c218969c3403c97182371
Jhoni Giacomolli <edicao@microsite.net.br>
2023-10-05 06:45:44 -0300

    test: implement upload and remove files on workspace update controller
    Signed-off-by: Jhoni Giacomolli <edicao@microsite.net.br>

--- src/controllers/workspace/workspace-upate/workspace-update-controller.spec.ts
+++ src/controllers/workspace/workspace-upate/workspace-update-controller.spec.ts
@@ -2,6 +2,7 @@ import { httpResponse } from '@/helpers/http'
 import { makeFakeRequest } from '@/mocks/http'
 import { WorkspaceStub } from '@/mocks/workspace/wokrspace-stub'
 import { makeFakeWorkspace } from '@/mocks/workspace/workspace-fakes'
+import { FileManagerStub } from '@/mocks/file-manager/file-manager-stub'
 import { WorkspaceUpdateController } from './workspace-update-controller'
 import { BadRequestError, EmptyParamError, InvalidParamError } from '@/helpers/errors'
 
@@ -11,15 +12,62 @@ const makeSut = () => {
     body,
     params: { id },
   })
+  const { coverImage, profileImage, ...bodyWithoutImages } = body
+  const coverImageFile = {
+    filename: 'any-cover-filename.pgn',
+    mime: 'image/png',
+    extension: 'png',
+    data: 'only-image-data',
+  }
+  const profileImageFile = {
+    filename: 'any-cover-filename.pgn',
+    mime: 'image/png',
+    extension: 'png',
+    data: 'only-image-data',
+  }
+  const fakeRequestWithCoverImage = makeFakeRequest({
+    body: {
+      ...bodyWithoutImages,
+      profileImage: body.profileImage,
+    },
+    params: { id },
+    files: {
+      coverImage: coverImageFile,
+    },
+  })
+  const fakeRequestWithProfileImage = makeFakeRequest({
+    body: {
+      ...bodyWithoutImages,
+      coverImage: body.coverImage,
+    },
+    params: { id },
+    files: {
+      profileImage: profileImageFile,
+    },
+  })
+  const fakeRequestWithBothImages = makeFakeRequest({
+    body: bodyWithoutImages,
+    params: { id },
+    files: {
+      coverImage: coverImageFile,
+      profileImage: profileImageFile,
+    },
+  })
   const workspaceStub = new WorkspaceStub()
+  const fileManagerStub = new FileManagerStub()
   const sut = new WorkspaceUpdateController({
     workspace: workspaceStub,
+    fileManager: fileManagerStub,
   })
 
   return {
     sut,
     fakeRequest,
+    fakeRequestWithBothImages,
+    fakeRequestWithCoverImage,
+    fakeRequestWithProfileImage,
     workspaceStub,
+    fileManagerStub,
   }
 }
 
@@ -33,8 +81,9 @@ describe('WorkspaceUpdateController', () => {
     await expect(output).rejects.toThrow(new EmptyParamError('id'))
   })
 
-  const { sut, fakeRequest, workspaceStub } = makeSut()
   test('Should return invalid param error if workspace provided is invalid', async () => {
+    const { sut, fakeRequest, workspaceStub } = makeSut()
+
     jest.spyOn(workspaceStub, 'getById').mockReturnValueOnce(Promise.resolve(undefined))
 
     const output = sut.handle(fakeRequest)
@@ -68,6 +117,87 @@ describe('WorkspaceUpdateController', () => {
     const output = sut.handle(fakeRequestWithWrongTitleBodyParam)
 
     await expect(output).rejects.toThrow(new InvalidParamError('members must have to be a array'))
+  })
+
+  test('Should not calls remove image method if have respective file and not have old image', async () => {
+    const { sut, fakeRequestWithCoverImage, workspaceStub, fileManagerStub } = makeSut()
+
+    jest.spyOn(workspaceStub, 'getById').mockReturnValueOnce(Promise.resolve({
+      ...makeFakeWorkspace(),
+      coverImage: '',
+    }))
+    const removerSpy = jest.spyOn(fileManagerStub, 'removeImage')
+
+    await sut.handle(fakeRequestWithCoverImage)
+
+    expect(removerSpy).not.toHaveBeenCalledWith(makeFakeWorkspace().coverImage)
+    expect(removerSpy).toHaveBeenCalledTimes(0)
+  })
+
+  test('Should upload cover image and remove old cover image if a respective file is provided', async () => {
+    const { sut, fakeRequestWithCoverImage, workspaceStub, fileManagerStub } = makeSut()
+    const { id, ...params } = fakeRequestWithCoverImage.body
+    const getByIdSpy = jest.spyOn(workspaceStub, 'getById')
+    const setByIdSpy = jest.spyOn(workspaceStub, 'setById')
+    const uploaderSpy = jest.spyOn(fileManagerStub, 'uploadImage')
+    const removerSpy = jest.spyOn(fileManagerStub, 'removeImage')
+
+    await sut.handle(fakeRequestWithCoverImage)
+
+    expect(getByIdSpy).toHaveBeenCalledWith(fakeRequestWithCoverImage.params.id)
+    expect(removerSpy).toHaveBeenCalledWith(makeFakeWorkspace().coverImage)
+    expect(removerSpy).toHaveBeenCalledTimes(1)
+    expect(uploaderSpy).toHaveBeenCalledWith(fakeRequestWithCoverImage.files.coverImage)
+    expect(uploaderSpy).toHaveBeenCalledTimes(1)
+    expect(setByIdSpy).toHaveBeenCalledWith(fakeRequestWithCoverImage.params.id, {
+      ...params,
+      coverImage: '/uploads/any-file-uploaded.png',
+    })
+  })
+
+  test('Should upload profile image and remove old cover image if a respective file is provided', async () => {
+    const { sut, fakeRequestWithProfileImage, workspaceStub, fileManagerStub } = makeSut()
+    const { id, ...params } = fakeRequestWithProfileImage.body
+    const getByIdSpy = jest.spyOn(workspaceStub, 'getById')
+    const setByIdSpy = jest.spyOn(workspaceStub, 'setById')
+    const uploaderSpy = jest.spyOn(fileManagerStub, 'uploadImage')
+    const removerSpy = jest.spyOn(fileManagerStub, 'removeImage')
+
+    await sut.handle(fakeRequestWithProfileImage)
+
+    expect(getByIdSpy).toHaveBeenCalledWith(fakeRequestWithProfileImage.params.id)
+    expect(removerSpy).toHaveBeenCalledWith(makeFakeWorkspace().profileImage)
+    expect(removerSpy).toHaveBeenCalledTimes(1)
+    expect(uploaderSpy).toHaveBeenCalledWith(fakeRequestWithProfileImage.files.profileImage)
+    expect(uploaderSpy).toHaveBeenCalledTimes(1)
+    expect(setByIdSpy).toHaveBeenCalledWith(fakeRequestWithProfileImage.params.id, {
+      ...params,
+      profileImage: '/uploads/any-file-uploaded.png',
+    })
+  })
+
+  test('Should upload both images and remove old images if a respective files are provided', async () => {
+    const { sut, fakeRequestWithBothImages, workspaceStub, fileManagerStub } = makeSut()
+    const { id, ...params } = fakeRequestWithBothImages.body
+    const getByIdSpy = jest.spyOn(workspaceStub, 'getById')
+    const setByIdSpy = jest.spyOn(workspaceStub, 'setById')
+    const uploaderSpy = jest.spyOn(fileManagerStub, 'uploadImage')
+    const removerSpy = jest.spyOn(fileManagerStub, 'removeImage')
+
+    await sut.handle(fakeRequestWithBothImages)
+
+    expect(getByIdSpy).toHaveBeenCalledWith(fakeRequestWithBothImages.params.id)
+    expect(removerSpy).toHaveBeenCalledWith(makeFakeWorkspace().coverImage)
+    expect(removerSpy).toHaveBeenCalledWith(makeFakeWorkspace().profileImage)
+    expect(removerSpy).toHaveBeenCalledTimes(2)
+    expect(uploaderSpy).toHaveBeenCalledWith(fakeRequestWithBothImages.files.coverImage)
+    expect(uploaderSpy).toHaveBeenCalledWith(fakeRequestWithBothImages.files.profileImage)
+    expect(uploaderSpy).toHaveBeenCalledTimes(2)
+    expect(setByIdSpy).toHaveBeenCalledWith(fakeRequestWithBothImages.params.id, {
+      ...params,
+      coverImage: '/uploads/any-file-uploaded.png',
+      profileImage: '/uploads/any-file-uploaded.png',
+    })
   })
 
   test('Should calls setById method of repository with correct values', async () => {
